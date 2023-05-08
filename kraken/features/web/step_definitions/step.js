2c69f29acfe828e92d65a3547094d6168c6f9e83
Fredy Alarcon <f.alarconf@uniandes.edu.co>
2023-05-07 23:21:10 -0500

    Se crearon lso escenarios ES014 AL ES018 según archivo README.md

--- kraken/features/web/step_definitions/step.js
+++ kraken/features/web/step_definitions/step.js
@@ -313,3 +313,42 @@ Then('I search post {kraken-string} in the list', async function (post) {
     let elementLinks = await this.driver.$$(`.posts-list li > a`);
     return await elementLinks[index].click();
 });
+
+When('I click general', async function () {
+    let element = await this.driver.$("section.gh-nav-body").$('a[href="#/settings/general/"]');
+    return await element.click();
+});
+
+When('I click expand title and description', async function () {
+    let element = await this.driver.$$('.gh-setting-first > .gh-setting-action > button')[0];
+    return await element.click();
+});
+
+When('I type title {kraken-string}', async function (title) {
+    let element = await this.driver.$(".ember-text-field.gh-input.ember-view");
+    return await element.setValue(title);
+});
+
+When('I type description {kraken-string}', async function (description) {
+    let element = await this.driver.$$(".ember-text-field.gh-input.ember-view")[1];
+    return await element.setValue(description);
+});
+
+When('I user save settings', async function () {
+    let element = await this.driver.$(".gh-canvas-header > .view-actions > button");
+    return await element.click();
+});
+
+Then('I title was updated', async function () {
+    try {
+        let element = await this.driver.$(".gh-nav-menu-details-blog");
+        return assert.equal("Nuevo Título", element.getText());
+    } catch (error) {
+        return error;
+    }
+});
+
+When('And I user click on upload image', async function () {
+    let element = await this.driver.$('.gh-setting-first > .gh-setting-action > div > span > input[type="file"]').selectFile('/fixtures/kraken-icon.png', { force: true });
+    return await element.click();
+});
