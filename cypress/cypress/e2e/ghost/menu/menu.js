88b507f94db04ba72396d8a9d41b915ee812a86a
Humberto Maury <h.maury@uniandes.edu.co>
2023-05-10 09:42:25 -0500

    screenshots

--- cypress/cypress/e2e/ghost/menu/menu.js
+++ cypress/cypress/e2e/ghost/menu/menu.js
@@ -1,5 +1,11 @@
 export class Menu {
 
+  scenario = ''
+
+  constructor(scenario = '') {
+    this.scenario = scenario;
+  }
+
   get postLink() {
     return cy.get('li > a[href="#/posts/"]');
   } 
@@ -40,63 +46,67 @@ export class Menu {
     return cy.get('li > a[href="#/pages/"]');
   }
 
-  constructor() {}
-
   when_user_navigate_to_posts = () => {
     this.postLink.click();
+    cy.screenshot(`${this.scenario}/navigate_to_post`);
   };
 
   when_user_expand_profile = () => {
     this.profileDropdown.click();
+    cy.screenshot(`${this.scenario}/navigate_to_profile`);
   };
 
   when_user_logout = () => {
     this.logoutButton.click();
+    cy.screenshot(`${this.scenario}/click_on_logout`);
   };
 
   when_user_click_profile = () => {
     this.profileButton.click();
+    cy.screenshot(`${this.scenario}/click_on_profile`);
   };
 
   when_user_navigate_to_general = () => {
     this.generalLink.click();
+    cy.screenshot(`${this.scenario}/navigate_to_general`);
   };
 
   when_user_navigate_to_code_injection = () => {
     this.codeInjectionLink.click();
+    cy.screenshot(`${this.scenario}/navigate_to_code_injection`);
   };
 
   when_user_navigate_to_site = () => {
     this.siteLink.click();
+    cy.screenshot(`${this.scenario}/navigate_to_site`);
   };
 
   when_user_navigate_to_staff = () => {
     this.staffLink.click();
+    cy.screenshot(`${this.scenario}/navigate_to_staff`);
   };
 
   when_user_navigate_to_tags = () => {
     this.tagLink.click();
+    cy.screenshot(`${this.scenario}/navigate_to_tags`);
   };
 
   when_user_navigate_to_pages = () => {
     this.pageLink.click();
-  };
-
-  when_user_navigate_to_pages = () => {
-    cy.get("section.gh-nav-body").within(() => {
-      cy.get('a[href="#/pages/"]').click();
-    });
+    cy.screenshot(`${this.scenario}/navigate_to_pages`);
   };
 
   when_user_navigate_to_config = () => {
     cy.get("section.gh-nav-body").within(() => {
       cy.get('a[href="#/settings/design/"]').click();
+      cy.screenshot(`${this.scenario}/navigate_to_config`);
     });
   }
 
   when_user_navigate_to_view_site = () => {
     cy.get("section.gh-nav-body").within(() => {
       cy.get('a[href="#/site/"]').click();
+      cy.screenshot(`${this.scenario}/navigate_to_view_site`);
     });
   };
 }
