aaa42964fa15e707fedd35abf1e317a3a5adc568
SantiagoRC <ds.rondon@uniandes.edu.co>
2023-05-13 16:33:19 -0500

    Screenshots Page y Config GhostV3

--- cypress/cypress/e2e/ghost3/config/ES019-config-nav-main.cy.js
+++ cypress/cypress/e2e/ghost3/config/ES019-config-nav-main.cy.js
@@ -12,32 +12,29 @@ describe("Testion new menus", () => {
   let _configObject;
   beforeEach(()=>{
     // GIVEN: usuario autenticado y navego hasta pages
-    _siteObject = new Site("http://localhost:2368/ghost/#/signin");
+    _siteObject = new Site("ES019");
     _siteObject.given_user_visit_ghost();
 
-    _loginObject = new Login();
+    _loginObject = new Login("ES019");
     _loginObject.when_user_enter_credentials_and_click_on_login();
 
-    menuObject = new Menu();
+    menuObject = new Menu("ES019");
     menuObject.when_user_navigate_to_pages();
 
-    _pageObject = new Page();
+    _pageObject = new Page("ES019");
     _pageObject.when_user_click_on_new_page();
     _pageObject.when_user_type_title_and_content();
     _pageObject.when_user_publish_page();
+    _pageObject.when_user_get_back_from_the_page();
   });
 
   it("Create new page menu en la navegacion principal", () => {
-    menuObject = new Menu();
-    _pageObject = new Page();
-    _configObject = new Config();
-      
-    // WHEN: usuario obtiene el link de la pagina
-    let url =_pageObject.when_user_get_page_link();
-    
+    menuObject = new Menu("ES019");
+    _pageObject = new Page("ES019");
+    _configObject = new Config("ES019");
+
     menuObject.when_user_navigate_to_config();
     _configObject.when_user_create_new_menu_on_primary_navigation();
-    //menuObject.when_user_navigate_to_view_site();
     _configObject.then_the_menu_was_created();
   });
 
--- cypress/cypress/e2e/ghost3/config/config.js
+++ cypress/cypress/e2e/ghost3/config/config.js
@@ -1,13 +1,17 @@
 export class Config {
-    constructor() {}
+    scenario = ''
+
+    constructor(scenario = '') {
+        this.scenario = scenario;
+    }
     
     when_user_create_new_menu_on_primary_navigation = (url) => {
         cy.get('input[placeholder="Label"]').last().type("Nueva Pagina")
         cy.wait(100)
         cy.get('input.ember-text-field').first().type("nueva-pagina")
-        cy.get('button.gh-blognav-add').first().click();
         cy.wait(500)
         cy.get('button.gh-btn-blue').click();
+        cy.screenshot(`${this.scenario}/create_primary_menu`);
         
     };
 
@@ -15,15 +19,15 @@ export class Config {
         cy.get('input[placeholder="Label"]').last().type("Nueva Pagina")
         cy.wait(100)
         cy.get('input.ember-text-field').last().type("nueva-pagina")
-        cy.get('button.gh-blognav-add').last().click();
         cy.wait(500)
         cy.get('button.gh-btn-blue').click();
         
+        cy.screenshot(`${this.scenario}/create_second_menu`);
     };
     
     then_the_menu_was_created = () => {
         cy.get('button.gh-btn-green').should('exist');
-           
+        cy.screenshot(`${this.scenario}/menu_created`);   
         //document.querySelector(" div.site-nav-right")
         
     };
