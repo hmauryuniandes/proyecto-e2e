36d777a7fcdc1c4447eb302c9a9d2f5b628cb43b
SantiagoRC <ds.rondon@uniandes.edu.co>
2023-05-13 16:21:49 -0500

    Page y config para ghost 4

--- cypress/cypress/e2e/ghost4/config/config.js
+++ cypress/cypress/e2e/ghost4/config/config.js
@@ -1,29 +1,33 @@
 export class Config {
-    constructor() {}
+    scenario = ''
+
+    constructor(scenario = '') {
+        this.scenario = scenario;
+    }
     
     when_user_create_new_menu_on_primary_navigation = (url) => {
-        cy.get('input[placeholder="Label"]').last().type("Nueva Pagina")
+        cy.get('input[placeholder="Label"]').first().clear().type("Nueva Pagina")
         cy.wait(100)
-        cy.get('input.ember-text-field').first().type("nueva-pagina")
-        cy.get('button.gh-blognav-add').first().click();
+        cy.get('span.gh-blognav-url > input.ember-text-field').first().clear().type("nueva-pagina")
         cy.wait(500)
-        cy.get('button.gh-btn-blue').click();
+        cy.get('button.gh-btn-primary').click();
+        cy.screenshot(`${this.scenario}/create_primary_menu`);
         
     };
 
     when_user_create_new_menu_onsecondary_navigation = (url) => {
         cy.get('input[placeholder="Label"]').last().type("Nueva Pagina")
         cy.wait(100)
-        cy.get('input.ember-text-field').last().type("nueva-pagina")
-        cy.get('button.gh-blognav-add').last().click();
+        cy.get('span.gh-blognav-url > input.ember-text-field').last().type("nueva-pagina")
         cy.wait(500)
-        cy.get('button.gh-btn-blue').click();
+        cy.get('button.gh-btn-primary').click();
         
+        cy.screenshot(`${this.scenario}/create_second_menu`);
     };
     
     then_the_menu_was_created = () => {
         cy.get('button.gh-btn-green').should('exist');
-           
+        cy.screenshot(`${this.scenario}/menu_created`);   
         //document.querySelector(" div.site-nav-right")
         
     };
